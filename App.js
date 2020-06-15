59eac4a33a4df395791d4453fd401246493bf48d
Piotr <piotr.wach122@gmail.com>
2020-06-15 16:15:11 +0200

    Add loading screen

--- App.js
+++ App.js
@@ -1,19 +1,87 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, MaskedViewIOS, Animated } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+export default class App extends React.Component {
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  state = {
+    loadingProgress: new Animated.Value(0),
+    animationDone: false
+  }
+
+  componentDidMount() {
+    Animated.timing(this.state.loadingProgress, {
+      toValue: 100,
+       duration: 1000,
+       useNativeDriver: true,
+       delay: 400
+    }).start(() => {
+      this.setState({animationDone: true});
+    });
+  }
+
+  render(){
+    const colorLayer = this.state.animationDone ? null : (
+      <View style={[StyleSheet.absoluteFill, {backgroundColor: "#195465"}]} />
+    );
+
+    const whiteLayer = this.state.animationDone ? null : (
+      <View style={[StyleSheet.absoluteFill, {backgroundColor: "#FFF"}]} />
+    );
+
+    const imageScale = {
+      transform: [
+        {
+          scale: this.state.loadingProgress.interpolate({
+            inputRange: [0, 15, 100],
+            outputRange: [0.1, 0.06, 16]
+          })
+        }
+      ]
+    };
+
+    const opacity = {
+      opacity: this.state.loadingProgress.interpolate({
+        inputRange: [0, 25, 50],
+        outputRange: [0, 0, 1],
+        extrapolate: "clamp"
+      })
+    }
+
+
+    return(
+      <View style={{ flex: 1 }}>
+        {colorLayer}
+
+        <MaskedViewIOS 
+          style={{ flex: 1 }} 
+          maskElement={
+
+            <View style={styles.centered}>
+            
+              <Animated.Image
+                source={require("./assets/logo.png")}
+                style={[{ width: 1000 }, imageScale]}
+                resizeMode="contain"
+
+                />
+
+            </View>
+          }
+        >
+        {whiteLayer}
+        <Animated.View style={opacity, styles.centered}>
+          <Text>Your App goes here!!</Text>
+        </Animated.View>
+
+        </MaskedViewIOS>
+      </View>
+    );  
+    }
+
+    const styles = StyleSheet.create({
+      centered: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+      }
+    });
